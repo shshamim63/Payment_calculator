45d226047cb60bf413a381644c9c3d1173d6df0b
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-01-05 13:51:06 +0600

    initialized payment method to get the payment list

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -4,6 +4,13 @@ class PaymentController < ApplicationController
   end
 
   def calculate
+    @payments = Payment.final_payment_list(params[:loan_amount].to_f,
+                                            params[:terms].to_f,
+                                            params[:interest].to_f,
+                                            Date.parse(params[:datetime]),
+                                            params[:interest_only]
+                                          )
     
+    render 'welcome/index'
   end
 end
