30cf64e5f95740c274c46363bf806a01fcb7bf5b
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-01-08 17:23:28 +0600

    Fixed corner cases to get data based on modified interest_only field

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,19 +1,18 @@
 class Payment < ApplicationRecord
   def self.create_instance(cal_amount, cal_interest, cal_pmt, cal_principal, cal_day, cal_end_balance)
     payment = self.new start_balance: cal_amount,
-                           interest: cal_interest,
-                           total_pmt: cal_pmt,
-                           principal: cal_principal,
-                           day: cal_day,
-                           end_balance: cal_end_balance
-        
+                       interest: cal_interest,
+                       total_pmt: cal_pmt,
+                       principal: cal_principal,
+                       day: cal_day,
+                       end_balance: cal_end_balance        
   end
   
   def self.final_payment_list (amount, terms, rate, date, interest_only)
     ans = []
     years = terms.to_f/12
-    
-    effected_term = terms
+    effected_term = interest_only == 'Yes' ? terms - 3 : terms
+    pmt = Payment.total_pmt(amount, rate, years, effected_term)
     (0...terms).each do |term|
       if interest_only == 'Yes' and term < 3
         interest = self.interest_monthly(amount, rate, terms).round(2)
@@ -21,7 +20,6 @@ class Payment < ApplicationRecord
                                       0.00, date.next_month, amount)
         ans.push(payment)
       else
-        pmt = Payment.total_pmt(amount, rate, years, effected_term)
         interest = self.interest_monthly(amount, rate, effected_term).round(2)
         principal = (pmt - interest).round(2)
         end_balance = (amount - principal).round(2) >= 0 ? (amount - principal).round(2) : 0
@@ -32,7 +30,6 @@ class Payment < ApplicationRecord
         amount = end_balance
       end
       date = date.next_month
-      effected_term -= 1
     end
     ans
   end
