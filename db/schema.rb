a8c0830f4cba807c5628b9610ce3b83ccf13f79d
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-01-28 15:11:29 +0600

    Generated migration for loan reference in payments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_101817) do
+ActiveRecord::Schema.define(version: 2021_01_28_090711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_01_27_101817) do
     t.decimal "end_balance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "loan_id"
+    t.index ["loan_id"], name: "index_payments_on_loan_id"
   end
 
 end
